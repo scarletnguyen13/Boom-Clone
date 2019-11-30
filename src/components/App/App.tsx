22e9ed98f0a39b81468a4df6ec67dc59cdd957f1
TuanPham303 <phamcongtuan303@gmail.com>
2019-11-30 11:40:58 -0800

    setup

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './App.css';
+import { Logo } from '..';
+
+const App: React.FC = () => {
+  return (
+    <Logo />
+  );
+}
+
+export default App;
---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Logo: React.FC = () => {
+  return (
+    <div>Boom</div>
+  );
+}
+
+export default Logo;
